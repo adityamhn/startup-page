2ab967513f489fa683e5877df58392404583275d
adityamhn <aditya530026@gmail.com>
2020-11-10 16:05:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "19em";
+  document.getElementById("main").style.marginLeft = "5em";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0em";
+}
